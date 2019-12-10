6ee928c4dc9607de7ff37004f1b2b620b8a2c6d6
Ivan Zakhartchouk <izakhartchouk@suvoda.com>
2019-12-10 15:29:20 +0300

    Added API requests wrapper
    - added loading indicator
    - simulated request delay

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
     setEditMode: (editMode: boolean) => void;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
     setEditMode,
     activity: initialFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
     const initializeForm = () => {
         if (initialFormState) {
@@ -92,7 +94,13 @@ const ActivityForm: React.FC<IProps> = ({
                     placeholder='Venue'
                     value={activity.venue}
                 />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button
+                    loading={submitting}
+                    floated='right'
+                    positive
+                    type='submit'
+                    content='Submit'
+                />
                 <Button
                     onClick={() => setEditMode(false)}
                     floated='right'
