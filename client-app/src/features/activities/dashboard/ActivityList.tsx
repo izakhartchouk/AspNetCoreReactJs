aad2109e111dca7bb5a7e6859dc0e22e33c2bc68
Ivan Zakhartchouk <izakhartchouk@suvoda.com>
2019-12-10 23:45:54 +0300

    Added MobX and MobX Lite packages
    - added ActivityStore
    - refactored activities components to use ActivityStore

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,29 +1,19 @@
-import React, { SyntheticEvent } from "react";
+import React, { useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
+import { observer } from "mobx-react-lite";
+import ActivityStore from "../../../app/stores/ActivityStore";
 
-interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    submitting: boolean;
-    target: string;
-}
+export const ActivityList: React.FC = () => {
+    const activityStore = useContext(ActivityStore);
+    const { activitiesByDate, selectActivity, deleteActivity, submitting, target } = activityStore;
 
-export const ActivityList: React.FC<IProps> = ({
-    activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-    target
-}) => {
     return (
         <Segment clearing>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Header as="a">{activity.title}</Item.Header>
                             <Item.Meta>{activity.date}</Item.Meta>
                             <Item.Description>
                                 <div>{activity.description}</div>
@@ -34,17 +24,17 @@ export const ActivityList: React.FC<IProps> = ({
                             <Item.Extra>
                                 <Button
                                     onClick={() => selectActivity(activity.id)}
-                                    floated='right'
-                                    content='View'
-                                    color='blue'
+                                    floated="right"
+                                    content="View"
+                                    color="blue"
                                 ></Button>
                                 <Button
                                     name={activity.id}
                                     loading={target === activity.id && submitting}
                                     onClick={e => deleteActivity(e, activity.id)}
-                                    floated='right'
-                                    content='Delete'
-                                    color='red'
+                                    floated="right"
+                                    content="Delete"
+                                    color="red"
                                 ></Button>
                                 <Label basic content={activity.category}></Label>
                             </Item.Extra>
@@ -56,4 +46,4 @@ export const ActivityList: React.FC<IProps> = ({
     );
 };
 
-export default ActivityList;
+export default observer(ActivityList);
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,16 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/ActivityStore";
 
 interface IProps {
     activity: IActivity;
-    setEditMode: (editMode: boolean) => void;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean;
 }
 
-const ActivityForm: React.FC<IProps> = ({
-    setEditMode,
-    activity: initialFormState,
-    createActivity,
-    editActivity,
-    submitting
-}) => {
+const ActivityForm: React.FC<IProps> = ({ activity: initialFormState }) => {
+    const activityStore = useContext(ActivityStore);
+    const { createActivity, editActivity, submitting, cancelFormOpen } = activityStore;
     const initializeForm = () => {
         if (initialFormState) {
             return initialFormState;
@@ -58,55 +51,50 @@ const ActivityForm: React.FC<IProps> = ({
             <Form onSubmit={handleSubmit}>
                 <Form.Input
                     onChange={handleInputChange}
-                    name='title'
-                    placeholder='Title'
+                    name="title"
+                    placeholder="Title"
                     value={activity.title}
                 />
                 <Form.TextArea
                     onChange={handleInputChange}
-                    name='description'
+                    name="description"
                     rows={2}
-                    placeholder='Description'
+                    placeholder="Description"
                     value={activity.description}
                 />
                 <Form.Input
                     onChange={handleInputChange}
-                    name='category'
-                    placeholder='Category'
+                    name="category"
+                    placeholder="Category"
                     value={activity.category}
                 />
                 <Form.Input
                     onChange={handleInputChange}
-                    name='date'
-                    type='datetime-local'
-                    placeholder='Date'
+                    name="date"
+                    type="datetime-local"
+                    placeholder="Date"
                     value={activity.date}
                 />
                 <Form.Input
                     onChange={handleInputChange}
-                    name='city'
-                    placeholder='City'
+                    name="city"
+                    placeholder="City"
                     value={activity.city}
                 />
                 <Form.Input
                     onChange={handleInputChange}
-                    name='venue'
-                    placeholder='Venue'
+                    name="venue"
+                    placeholder="Venue"
                     value={activity.venue}
                 />
                 <Button
                     loading={submitting}
-                    floated='right'
+                    floated="right"
                     positive
-                    type='submit'
-                    content='Submit'
+                    type="submit"
+                    content="Submit"
                 />
-                <Button
-                    onClick={() => setEditMode(false)}
-                    floated='right'
-                    type='button'
-                    content='Cancel'
-                />
+                <Button onClick={cancelFormOpen} floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
     );
