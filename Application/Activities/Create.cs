affdaee9e0852f4e5e00ecc3547aed6745217b50
Ivan Zakhartchouk <izakhartchouk@suvoda.com>
2019-12-18 18:36:11 +0300

    Added Validation to Create, Edit commands
    - registered validators in Startup

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -29,7 +29,15 @@ namespace Application.Activities
 
         public class CommandValidator : AbstractValidator<Command>
         {
-
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
+            }
         }
 
         public class Handler : IRequestHandler<Command>
